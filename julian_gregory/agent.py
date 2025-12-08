211bfa1a429801b8c7b1ae551ecb739fa1f73718
Keith Rozario <keithjosephrozario@gmail.com>
2025-12-08 17:38:17 +0800

    added summarization agent

--- julian_gregory/agent.py
+++ julian_gregory/agent.py
@@ -1,18 +1,49 @@
 from google.adk.agents import Agent
 from google.adk.apps.app import App
-from googleapiclient.discovery import build
+from google.adk.tools.agent_tool import AgentTool
+from .tools import get_todays_events, set_weather
 
-from .helper_funcs import get_creds
-from .tools import set_weather
+summary_agent = Agent(
+    name="summary_agent",
+    model="gemini-2.0-flash",
+    description=("An agent that provides a summary of the day's events"),
+    instruction=(
+"""
+You are a helpful assistant that will summarize the days meetings for a user. 
+
+Look through the events for the day and provide a useful summary of:
+
+1. All meetings and events taking place today
+2. Any free slots and time that are in the calendar
+3. Any last minute meetings that were booked after 5pm yesterday should be flagged.
+
+Here's an example summary:
+
+Summary: You have a busy day ahead with many back-2-back meetings. Remember to hydrate and take breaks.
+
+Meetings:
+
+1. Meeting with Brad. 9-10pm, <link to meeting>
+2. Project ABC team huddle 10.30-11am, <link to meeting>
+3. Lunch with Randy, <link to meeting>
+4. .....
+
+Late last night an invite for quick huddle at 2pm today was booked into your calendar by Edmund. You may not have read the invite.
+
+"""
+    ),
+    tools=[get_todays_events],
+)
+
 
 root_agent = Agent(
     name="julian_gregory_day",
     model="gemini-2.0-flash",
-    description=("Agent to answer questions about the time and weather in a city."),
+    description=("Julian is an agent that helps users with their Calendars"),
     instruction=(
-        "You are a helpful agent who can answer user questions about the time and weather in a city."
+        "You are a helpful calendar agent. You help users organize their calendars using the tools and subagents at your disposal."
     ),
-    tools=[set_weather],
+    tools=[AgentTool(agent=summary_agent)],
 )
 
 
--- julian_gregory/tools.py
+++ julian_gregory/tools.py
@@ -1,5 +1,7 @@
 
 from google.adk.tools.tool_context import ToolContext
+import datetime
+from zoneinfo import ZoneInfo
 from .helper_funcs import get_service
 
 
@@ -30,3 +32,40 @@ def set_weather(city: str, weather: str, tool_context: ToolContext) -> dict:
         "status": "success",
         "report": "All done"
     }
+
+
+def get_todays_events(tool_context: ToolContext) -> list[dict]:
+    """
+    Gets a list of events for today. 
+    Today is inferred between the system time and timeZone set on the calendar.
+
+    returns:
+        events: List of Dicts of the events
+    """
+
+
+    calendar_service, _ = get_service(tool_context)
+    time_zone = calendar_service.calendars().get(calendarId="primary").execute()['timeZone']
+    today = datetime.datetime.now(ZoneInfo(time_zone))
+
+    start_of_today = datetime.datetime(today.year, today.month, today.day, 0, 0, 0, tzinfo=ZoneInfo(time_zone))
+    start_of_tomorrow = start_of_today + datetime.timedelta(days=1)
+
+    # Format as RFC3339 timestamps
+    time_min = start_of_today.isoformat()
+    time_max = start_of_tomorrow.isoformat()
+
+    print(f'Getting events from {time_min} to {time_max}')
+
+    # Call the Calendar API events.list method
+    events_result = calendar_service.events().list(
+        calendarId='primary',
+        timeMin=time_min,
+        timeMax=time_max,
+        singleEvents=True,
+        orderBy='startTime'
+    ).execute()
+
+    events = events_result['items']
+
+    return events
